e34dcfc0d6a79391bc572145ae3d4bef1e71b439
Masih <masih.saldanha@gmail.com>
2023-03-22 19:24:38 -0300

    feat: add Scanners and basic logic

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o menor número: ");
+        final int smaller = sc.nextInt();
+
+        System.out.println("Digite o maior número: ");
+        final int bigger = sc.nextInt();
+
+        if (smaller > bigger) {
+            System.out.println("Primeiro número deve ser menor!");
+        } else if (smaller < 1) {
+            System.out.println("O número menor não pode ser menor '1'");
+        } else {
+            System.out.println("deu bom");
+        }
     }
 }
